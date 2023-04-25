8910c7b239edec458c3756ec766fa76327651c60
aieckxis <aleckxiskate@gmail.com>
2023-04-25 11:44:29 +0800

    pseudocode

---
+++ multiple line of text contents.py
@@ -0,0 +1,13 @@
+# Bernabe, Aleckxis Kate V.
+# BSCpE 1-4
+
+# Set the filename to "mylife.txt"
+# Using the with statement, open the file in write mode.
+# Ask the user to input their first name.
+# Loop until the user commands there are no more lines to enter
+# Ask the user to enter a line
+# Using the write() method, add the line to the file followed by a newline character.
+# Ask the user if there are more lines to enter.
+# Exit the loop if the user enters "n"
+# Ask for another line to enter if the user enters "y".
+# Ask again if the user enters anything else.
