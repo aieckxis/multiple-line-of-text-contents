841026a66937415004fbceab0993abeaaeb510d6
aieckxis <aleckxiskate@gmail.com>
2023-04-25 11:48:48 +0800

    opened the file and ask the use to input their first name

--- multiple line of text contents.py
+++ multiple line of text contents.py
@@ -2,8 +2,12 @@
 # BSCpE 1-4
 
 # Set the filename to "mylife.txt"
+file_name = "mylife.txt"
 # Using the with statement, open the file in write mode.
+with open(file_name, "w") as f:
 # Ask the user to input their first name.
+    first_name = input("Enter your first name: ")
+print(first_name) 
 # Loop until the user commands there are no more lines to enter
 # Ask the user to enter a line
 # Using the write() method, add the line to the file followed by a newline character.
