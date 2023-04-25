1b3a0ad40845ea295718c45174e0f6dfae33730c
aieckxis <aleckxiskate@gmail.com>
2023-04-25 11:54:39 +0800

    asked the user to enter line but has some error

--- multiple line of text contents.py
+++ multiple line of text contents.py
@@ -5,12 +5,14 @@
 file_name = "mylife.txt"
 # Using the with statement, open the file in write mode.
 with open(file_name, "w") as f:
-# Ask the user to input their first name.
+    # Ask the user to input their first name.
     first_name = input("Enter your first name: ")
-print(first_name) 
-# Loop until the user commands there are no more lines to enter
-# Ask the user to enter a line
-# Using the write() method, add the line to the file followed by a newline character.
+    # Loop until the user commands there are no more lines to enter
+    while True:
+        # Ask the user to enter a line
+        line = input("Enter line: ")
+        # Using the write() method, add the line to the file followed by a newline character.
+        file_name.write(line + "\n")
 # Ask the user if there are more lines to enter.
 # Exit the loop if the user enters "n"
 # Ask for another line to enter if the user enters "y".
